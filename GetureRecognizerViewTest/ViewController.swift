02e256cfad0445ff39508a705590db568506f758
Woody Liu <bestlifeforzhi@gmail.com>
2020-08-07 12:10:54 +0800

    practice done

--- GetureRecognizerViewTest/ViewController.swift
+++ GetureRecognizerViewTest/ViewController.swift
@@ -5,16 +5,77 @@
 //  Created by Woody Liu on 2020/8/7.
 //  Copyright © 2020 thisWhat. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    @IBOutlet var viewOne: UIView!
+    @IBOutlet var viewTwo: UIView!
+    @IBOutlet var viewThree: UIView!
+    var value:Int = 0
+    
+    var getureOne: UIGestureRecognizer = UITapGestureRecognizer()
+    var getureTwo: UIGestureRecognizer = UITapGestureRecognizer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getureOne.addTarget(self, action: #selector(viewOneAction))
+        getureOne.delegate = self
+        viewOne.addGestureRecognizer(getureOne)
+        getureTwo.addTarget(self, action: #selector(viewTwoAction))
+        getureTwo.delegate = self
+        viewTwo.addGestureRecognizer(getureTwo)
+        
+      
     }
 
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        let view = touch.view
+        
+        if gestureRecognizer == getureOne {
+            if view == viewTwo {
+                return true
+            }
+            return true
+        }
+        if view == viewTwo {
+            return true
+        }
+        
+        return false
+    }
+    
 
 }
+
+
+
+extension ViewController {
+    
+    @objc func viewOneAction(){
+        print("viewOne active")
+    }
+    
+    @objc func viewTwoAction(){
+        print("viewTwo active")
+    }
+    
+    @objc func viewThreeAction(){
+        print("viewThree active")
+        print("--------\(value)--------")
+        value += 1
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
+
 
